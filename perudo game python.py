39b9403493a3743d07115729650e4001ac9b4d5d
Ahmed HAYANE <ahmed.hayane@estiam.com>
2025-07-10 10:46:08 +0200

    Boucle principale du jeu

--- perudo game python.py
+++ perudo game python.py
@@ -10,10 +10,45 @@ def initialisation_game():      # initialisation du jeu
     return joueur
 
 def lancer_des(joueurs):  # lance les dés pour chaque joueur
-    print("Lancement des dés...") 
+    print("Lancement des dés...") #
     for joueur in joueurs:
         joueur["des"] = [random.randint(1, 6) for _ in joueur["des"]]
 
-def afficher_des(joueurs):
+def afficher_des(joueurs): # affiche les dés des joueur
     for joueur in joueurs:
         print(f"{joueur['nom']} a les dés : {joueur['des']}")
+
+def compter_occurrences(joueurs, valeur): # compte les valeurs des dés des joueurs
+    total = 0
+    for joueur in joueurs:
+        total += joueur["des"].count(valeur)
+    return total
+
+def tour_de_jeu(joueurs): # gere le tour de chaque joueur
+    enchere = None
+    joueur_actuel = 0
+    nb_joueurs = len(joueurs)
+
+joueurs = initialisation_game()
+joueur_actuel = 0
+enchere = None  # Initialisation de la variable enchere
+# boucle principale du jeu
+while True : 
+    joueur = joueurs[joueur_actuel]
+    print(f"C'est le tour de {joueur['nom']}")
+
+if enchere is None:
+    # la première enchère d'un joueur
+    nb = int(input("Entrez le nombre de dés pour la première enchère : "))
+    valeur = int(input("Entrez la valeur du dé pour la première enchère (1-6) : "))
+    enchere = (nb, valeur)
+else:
+    print(f"l'enchère actuelle est : {enchere[0]} dés de valeur {enchere[1]}")
+    while True:
+        nb = int(input("Entrez le nombre de dés pour la nouvelle enchère : "))
+        valeur = int(input("Entrez la valeur du dé pour la nouvelle enchère (1-6) : "))
+        if nb < enchere[0] or (nb == enchere[0] and valeur <= enchere[1]): # verification de l'enchere est valide est sup a 0
+            print("Enchère invalide, elle doit être supérieure à l'enchère précédente.")
+            continue
+        enchere = (nb, valeur)
+        break
